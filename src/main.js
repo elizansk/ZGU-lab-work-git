d3ec5ed85c9015aff81f3e0ecaa5689de1ada87a
elizansk <liza.savkova1@gmail.com>
2025-10-30 23:35:02 +0700

    Add addition multiply

--- src/main.js
+++ src/main.js
@@ -9,5 +9,11 @@ function divide(a, b) {
     }
     return a / b;
 }
+function multiply(a, b) {
+    return a * b;
+}
+
 
 console.log("2 + 3 =", add(2, 3));
+console.log("Result of multiplication:", multiply(2, 3));
+console.log("Result of division:", divide(10, 2));
